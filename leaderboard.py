783f55e8b1fa57e53c45c851e33b055a2c40b34c
Valentin Malykh <madrugado@yandex.ru>
2017-07-27 11:20:31 +0300

    FIX: adding assert on length of submission

--- leaderboard.py
+++ leaderboard.py
@@ -5,15 +5,21 @@ import sys
 import json
 import csv
 
-if len(sys.argv) != 2:
-    print("example: leaderboard.py <labels.csv>", file=sys.stderr)
+if len(sys.argv) < 2 or len(sys.argv) > 3:
+    print("example: leaderboard.py <labels.csv> [<data.json>]", file=sys.stderr)
     sys.exit(1)
 
-lines = sys.stdin.readlines()
+lines = None
+if len(sys.argv) == 2:
+    lines = sys.stdin.readlines()
+    line = "[" + ",".join(lines) + "]"
+else:
+    with open(sys.argv[2]) as f:
+        line = f.read()
+data = json.loads(line)
 
 users_bot_flags = {}
-for line in lines:
-    d = json.loads(line)
+for d in data:
     if d['evaluation'][0]['userId'] == 'Alice':
         users_bot_flags[d['dialogId']] = ( (int(d['evaluation'][0]['quality']), int(d['evaluation'][1]['quality'])) )
     else:
@@ -21,7 +27,7 @@ for line in lines:
 
 users_bot_predicted_probs = []
 users_bot_fact_labaels = []
-with open(sys.argv[1], 'r') as csvfile:
+with open(sys.argv[1], 'rt') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         dialog = int(row[0])
@@ -33,6 +39,8 @@ with open(sys.argv[1], 'r') as csvfile:
             users_bot_predicted_probs.append(float(row[2]))
         else:
             print("dialog %s not in dataset" % dialog, file=sys.stderr)
+
+assert len(users_bot_flags) == len(users_bot_fact_labaels) // 2 == len(users_bot_predicted_probs) // 2
 
 print(sys.argv[1], ": ", spearmanr(users_bot_fact_labaels, users_bot_predicted_probs).correlation)
 
