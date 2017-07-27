be657770fec03070df532da54210510c62947aba
Valentin Malykh <madrugado@yandex.ru>
2017-07-27 16:31:55 +0300

    FIX: typo

--- leaderboard.py
+++ leaderboard.py
@@ -26,21 +26,21 @@ for d in data:
         users_bot_flags[d['dialogId']] = ( (int(d['evaluation'][1]['quality']), int(d['evaluation'][0]['quality'])) )
 
 users_bot_predicted_probs = []
-users_bot_fact_labaels = []
+users_bot_fact_labels = []
 with open(sys.argv[1], 'rt') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         dialog = int(row[0])
         if dialog in users_bot_flags:
-            users_bot_fact_labaels.append(users_bot_flags[dialog][0])
+            users_bot_fact_labels.append(users_bot_flags[dialog][0])
             users_bot_predicted_probs.append(float(row[1]))
 
-            users_bot_fact_labaels.append(users_bot_flags[dialog][1])
+            users_bot_fact_labels.append(users_bot_flags[dialog][1])
             users_bot_predicted_probs.append(float(row[2]))
         else:
             print("dialog %s not in dataset" % dialog, file=sys.stderr)
 
-assert len(users_bot_flags) == len(users_bot_fact_labaels) // 2 == len(users_bot_predicted_probs) // 2
+assert len(users_bot_flags) == len(users_bot_fact_labels) // 2 == len(users_bot_predicted_probs) // 2
 
-print(sys.argv[1], ": ", spearmanr(users_bot_fact_labaels, users_bot_predicted_probs).correlation)
+print(sys.argv[1], ": ", spearmanr(users_bot_fact_labels, users_bot_predicted_probs).correlation)
 
